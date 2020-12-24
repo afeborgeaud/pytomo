b1e25da4230ad4df9ac00e196afc1378a9242385
Anselme F.E. Borgeaud <anselme@earth.sinica.edu.tw>
2020-12-24 13:16:50 +0800

    Add methods to save and load clusters

--- pytomo/dataset/sourceselection.py
+++ pytomo/dataset/sourceselection.py
@@ -11,6 +11,8 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import seaborn as sns
 import glob
+import pickle
+
 
 class SourceSelection:
     """Select seismic events to use for inversion
@@ -21,8 +23,8 @@ class SourceSelection:
             lon_min=-180, lon_max=180, dep_min=0, dep_max=800,
             Mw_min=0, Mw_max=10, dist_min=0,
             dist_max=180, n_within_dist=10,
-            start_date=datetime(1970,1,1),
-            end_date=datetime(2050,1,1)):
+            start_date=datetime(1970, 1, 1),
+            end_date=datetime(2050, 1, 1)):
         self.sac_files = sac_files
         self.lat_min = lat_min
         self.lat_max = lat_max
@@ -46,24 +48,24 @@ class SourceSelection:
         Args:
             event (pydsm.Event): seismic event
         """
-        iev = np.argwhere(self.dataset.events==event)[0]
+        iev = np.argwhere(self.dataset.events == event)[0]
         start, end = self.dataset.get_bounds_from_event_index(iev)
         stations = self.dataset.stations[start:end]
 
-        if (event.event_id[-1] != 'A' 
-            or event.latitude < self.lat_min
-            or event.latitude > self.lat_max
-            or event.longitude < self.lon_min
-            or event.longitude > self.lon_max
-            or event.depth < self.dep_min
-            or event.depth > self.dep_max
-            or event.mt.Mw < self.Mw_min
-            or event.mt.Mw > self.Mw_max
-            or event.centroid_time < self.start_date
-            or event.centroid_time > self.end_date):
+        if (event.event_id[-1] != 'A'
+                or event.latitude < self.lat_min
+                or event.latitude > self.lat_max
+                or event.longitude < self.lon_min
+                or event.longitude > self.lon_max
+                or event.depth < self.dep_min
+                or event.depth > self.dep_max
+                or event.mt.Mw < self.Mw_min
+                or event.mt.Mw > self.Mw_max
+                or event.centroid_time < self.start_date
+                or event.centroid_time > self.end_date):
             return False
         distances = np.array([event.get_epicentral_distance(station)
-                    for station in stations])
+                              for station in stations])
         n_within = (distances >= dist_min & distances <= dist_max).sum()
         if n_within < self.n_within_dist:
             return False
@@ -78,8 +80,8 @@ class SourceSelection:
         return depth_cat
 
     def _get_X(self, catalog):
-        X = np.zeros((len(catalog),3), dtype=np.float32)
-        X_cat = np.zeros((len(catalog),3), dtype=np.float32)
+        X = np.zeros((len(catalog), 3), dtype=np.float32)
+        X_cat = np.zeros((len(catalog), 3), dtype=np.float32)
         r = 6371
         for i, e in enumerate(catalog):
             x = np.radians(e.longitude + 180.) * r
@@ -95,7 +97,7 @@ class SourceSelection:
         scaler = StandardScaler()
         scaler.fit(X_cat)
         X_scaled = scaler.transform(X_cat)
-        X_scaled[:,2] *= 2
+        X_scaled[:, 2] *= 2
         dists_max = []
         found = False
         print(X_scaled.shape)
@@ -107,8 +109,8 @@ class SourceSelection:
             centers = scaler.inverse_transform(kmeans.cluster_centers_)
             dist_max = 0.
             for j, label in enumerate(kmeans.labels_):
-                dist = np.sqrt(np.dot(X[j,:2]-centers[label,:2],
-                            X[j,:2]-centers[label,:2]))
+                dist = np.sqrt(np.dot(X[j, :2] - centers[label, :2],
+                                      X[j, :2] - centers[label, :2]))
                 if dist > dist_max:
                     dist_max = dist
             dists_max.append(dist_max)
@@ -121,7 +123,7 @@ class SourceSelection:
         # plt.plot(list(range(1, max_clusters)), dists_max)
         # plt.show()
         centers = scaler.inverse_transform(kmeans_best.cluster_centers_)
-        centers[:,:2] = np.degrees(centers[:,:2] / 2. / 6371.)
+        centers[:, :2] = np.degrees(centers[:, :2] / 2. / 6371.)
 
         return kmeans_best.labels_, centers
 
@@ -143,9 +145,9 @@ class SourceSelection:
         return df
 
     def plot(
-        self, catalog, lon_min=-180, lon_max=180, lat_min=90, lat_max=90,
-        projection='robin', lon_0=-180, cluster_labels=None,
-        cmap=None):
+            self, catalog, lon_min=-180, lon_max=180, lat_min=90, lat_max=90,
+            projection='robin', lon_0=-180, cluster_labels=None,
+            cmap=None):
         fig, ax = plt.subplots()
 
         m = Basemap(resolution='i', projection=projection, lon_0=lon_0,
@@ -156,7 +158,7 @@ class SourceSelection:
         m.fillcontinents(color='wheat', lake_color='white')
 
         m.drawparallels(
-            np.arange(-80., 81., 20.),labels=[True, False, False, False],
+            np.arange(-80., 81., 20.), labels=[True, False, False, False],
             labelstyle='+/-', fontsize=10)
         m.drawmeridians(
             np.arange(-180., 181., 20.), labels=[False, False, False, True],
@@ -181,7 +183,7 @@ class SourceSelection:
         plt.savefig('map.pdf', bbox_inches='tight')
 
     def plot_cartesian(self, df, palette=None):
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8,5))
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 5))
         n_labels = len(df.label.unique())
         nh = 5
         ns = 3
@@ -198,29 +200,70 @@ class SourceSelection:
         ax2.set(xlabel='Depth (km)', ylabel='Latitude (deg)')
         plt.savefig('clusters_cart.pdf', bbox_inches='tight')
 
-        
+def get_clusters_as_list(labels, events):
+    """Return a list of list of Event
+
+    Args:
+        labels (ndarray): cluster labels
+        events (list of Event): events
+
+    Returns:
+        list ot list of Event: list of event clusters
+
+    """
+    n_cluster = labels.max()
+    clusters = [[] for i in range(n_cluster)]
+    for label in labels:
+        clusters[label].append(events[label])
+    return [cluster for cluster in clusters if len(cluster) > 0]
+
+
+def save_event_clusters(path, event_clusters):
+    """Save the event clusters using pickle.dump().
+
+        Args:
+            path (str): name of the output file.
+            event_clusters (list of list of Events): event clusters.
+        """
+    with open(path, 'wb') as f:
+        pickle.dump(event_clusters, f)
+
+def load_event_clusters(path):
+    """Read event clusters using pickle.load().
+
+    Args:
+        path (str): name of the file that contains the clusters
+
+    Returns:
+        list of list of Event: event clusters.
+
+    """
+    with open(path, 'rb') as f:
+        clusters = pickle.load(path)
+    return clusters
+
 if __name__ == '__main__':
     catalog = cmtcatalog.read_catalog()
-    
+
     dep_min = 150
     dep_max = 800
     dist_min = 15
     dist_max = 25
     Mw_min = 5.3
     Mw_max = 7.
-    start_date = datetime(2000,1,1)
+    start_date = datetime(2000, 1, 1)
 
     sac_files = glob.glob(
         '/mnt/doremi/anpan/inversion/MTZ_JAPAN/DATA/20*/*T')
 
-    selector = source_selection(
+    selector = SourceSelection(
         sac_files, dep_min=dep_min, dep_max=dep_max, Mw_min=Mw_min,
         Mw_max=Mw_max, dist_min=dist_min, dist_max=dist_max,
         start_date=start_date)
     catalog_filt = np.array(
         [event for event in selector.dataset.events
-        if selector.select(event)])
-    
+         if selector.select(event)])
+
     print('len(catalog)={}'.format(catalog_filt.shape))
 
     # exclude Philippines and Aleutians eqs
@@ -233,9 +276,9 @@ if __name__ == '__main__':
 
     _, counts = np.unique(cluster_labels, return_counts=True)
     cluster_centers_keep = cluster_centers[counts >= 2]
-    catalog_keep = [e for i,e in enumerate(catalog_filt)
+    catalog_keep = [e for i, e in enumerate(catalog_filt)
                     if counts[cluster_labels[i]] >= 2]
-    cluster_labels_keep = [label for i,label in enumerate(cluster_labels)
+    cluster_labels_keep = [label for i, label in enumerate(cluster_labels)
                            if counts[cluster_labels[i]] >= 2]
 
     print("n_events={}\nn_clusters={}"
@@ -252,3 +295,6 @@ if __name__ == '__main__':
     df.index = list(range(len(df)))
     df.to_csv('clusters.txt', sep=' ')
     selector.plot_cartesian(df)
+
+    event_clusters = get_clusters_as_list(cluster_labels_keep, catalog_keep)
+    save_event_clusters('event_cluster.pkl', event_clusters)
